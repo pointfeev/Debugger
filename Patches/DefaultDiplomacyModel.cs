7b0185ecfcff2ddd4a1039640f6d931f1dd66b5a
pointfeev <zehmeen@gmail.com>
2021-10-11 00:47:48 -0400

    performance fix

--- Patches/DefaultDiplomacyModel.cs
+++ Patches/DefaultDiplomacyModel.cs
@@ -1,6 +1,4 @@
 ﻿using HarmonyLib;
-using System;
-using System.Reflection;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 
@@ -11,20 +9,12 @@ namespace Debugger
     {
         [HarmonyPrefix]
         [HarmonyPatch("GetHeroesForEffectiveRelation")]
-        public static bool GetHeroesForEffectiveRelation(DefaultDiplomacyModel __instance, Hero hero1, Hero hero2, out Hero effectiveHero1, out Hero effectiveHero2)
+        public static bool GetHeroesForEffectiveRelation(Hero hero1, Hero hero2, out Hero effectiveHero1, out Hero effectiveHero2)
         {
             effectiveHero1 = hero1;
             effectiveHero2 = hero2;
-            if (!ReflectionUtils.IsMethodInCallStack(MethodBase.GetCurrentMethod()))
+            if (hero1 is null || hero2 is null)
             {
-                try
-                {
-                    __instance.GetHeroesForEffectiveRelation(hero1, hero2, out effectiveHero1, out effectiveHero2);
-                }
-                catch (Exception e)
-                {
-                    OutputUtils.DoOutputForException(e);
-                }
                 return false;
             }
             return true;
