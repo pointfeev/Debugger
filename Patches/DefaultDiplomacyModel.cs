914c5c6be4f6d80392459ca2258ea7953eba4ec7
pointfeev <zehmeen@gmail.com>
2021-10-11 00:28:54 -0400

    DefaultDiplomacyModel.GetHeroesForEffectiveRelation

---
+++ Patches/DefaultDiplomacyModel.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+
+namespace Debugger
+{
+    [HarmonyPatch(typeof(DefaultDiplomacyModel))]
+    public static class PatchDefaultDiplomacyModel
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("GetHeroesForEffectiveRelation")]
+        public static bool GetHeroesForEffectiveRelation(DefaultDiplomacyModel __instance, Hero hero1, Hero hero2, out Hero effectiveHero1, out Hero effectiveHero2)
+        {
+            effectiveHero1 = hero1;
+            effectiveHero2 = hero2;
+            if (!ReflectionUtils.IsMethodInCallStack(MethodBase.GetCurrentMethod()))
+            {
+                try
+                {
+                    __instance.GetHeroesForEffectiveRelation(hero1, hero2, out effectiveHero1, out effectiveHero2);
+                }
+                catch (Exception e)
+                {
+                    OutputUtils.DoOutputForException(e);
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
